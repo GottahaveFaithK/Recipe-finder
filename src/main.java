530eff4aa92f203c2bf64c0b12d96286ab93a658
Faith Kirkham <faithkirkham12@gmail.com>
2026-08-31 22:03:58 -0600

    Initial project setup

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    /*
+    ingredients
+        do I want to have amounts?
+    recipe
+        contains set of ingredients
+        any other information I want, or not at this current moment
+
+    needs CRUD operations
+    set containment problem
+
+     */
+}
